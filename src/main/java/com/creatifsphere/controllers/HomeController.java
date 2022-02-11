048a83817735e6d8dbd401fc9038619eee8356ff
Al Morris <al.morris.gtv@gmail.com>
2022-02-10 22:27:11 -0500

    added a home controller to grab web traffic

---
+++ src/main/java/com/creatifsphere/controllers/HomeController.java
@@ -0,0 +1,22 @@
+package com.creatifsphere.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class HomeController {
+
+	@RequestMapping("/")
+	public ResponseEntity<String> index() {
+		return ResponseEntity.ok("Greetings, What's today's mathmatics?")  ;
+	}
+	
+	@RequestMapping("/status")
+	public ResponseEntity<String> status() {
+		return ResponseEntity.ok("up")  ;
+	}
+}
